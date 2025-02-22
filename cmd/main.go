db75cac5bd91b6c5a3e695942189d6b292d796b6
dqx0 <38395087+dqx0@users.noreply.github.com>
2025-02-23 03:48:14 +0900

    add openapi

--- cmd/main.go
+++ cmd/main.go
@@ -5,19 +5,35 @@ import (
 	"net/http"
 
 	"github.com/dqx0/hitandblow/internal/server"
-
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "github.com/dqx0/hitandblow/docs" // generated docs package
 )
 
+// @title Hit and Blow API
+// @version 1.0.0
+// @description Hit and Blow GameのAPIドキュメント
+// @host localhost:8080
+// @BasePath /
 func main() {
 	r := gin.Default()
+
+	// Swagger UI を /swagger/index.html で公開
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	gameServer := server.NewGameServer()
 
+	// ヘルスチェック用エンドポイント
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "OK")
 	})
 
+	// ゲーム開始エンドポイント
 	r.POST("/games", gameServer.StartNewGame)
+
+	// 判定エンドポイント
 	r.POST("/games/:gameId/guess", gameServer.MakeGuess)
 
 	log.Println("サーバーを開始します: http://localhost:8080")
